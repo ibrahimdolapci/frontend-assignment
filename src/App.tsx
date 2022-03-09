b2b51b8949475a0360b67b0561bb0582b975a6c4
Ibrahim DOLAPCI <ibrahim@cofeapp.com>
2022-03-09 22:39:26 +0300

    setup leaflet library for map

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import {VesselTracks} from "./features/vessel-tracks";
 
 function App() {
     return (
         <div className="App">
-
+            <VesselTracks/>
         </div>
     );
 }
